7bcf0ba310f247383a395aa239fdd3936e91a1e4
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2020-08-27 15:06:27 -0400

    Implemented the  keyword

--- examples/ffi/lib/foreign.c
+++ examples/ffi/lib/foreign.c
@@ -3,3 +3,10 @@
 void test(machine *vm) {
     printf("This is a C foreign function!\n");
 }
+
+void __oak_add(machine *vm) {
+    int a = machine_pop(vm);
+    int b = machine_pop(vm);
+    printf("This should print %d => ", a + b);
+    machine_push(vm, a + b);
+}
--- examples/ffi/lib/foreign.go
+++ examples/ffi/lib/foreign.go
@@ -2,3 +2,10 @@
 func test(vm *machine) {
 	fmt.Println("This is a Go foreign function!")
 }
+
+func __oak_add(vm *machine) {
+	a := vm.pop()
+	b := vm.pop()
+	fmt.Printf("This should print %v => ", a+b)
+	vm.push(float64(a + b))
+}
